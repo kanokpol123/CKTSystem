e76970d620eefa0f07663611ad3ec17753a72768
Minero20 <pisit.namsiri@gmail.com>
2022-10-18 14:57:32 +0700

    14.57

--- Customer.cpp
+++ Customer.cpp
@@ -4,19 +4,7 @@ Customer::Customer(string telephone, string address, string firstname, string la
     this->telephone = telephone;
     this->address = address;
 }
-string Customer::getAddress()
-{
-    return address;
-}
-string Customer::getTelephone()
-{
-    return telephone;
-}
-void Customer::setAddress(string address)
-{
-    this->address = address;
-}
-void Customer::setTelephone(string telephone)
-{
-    this->telephone = telephone;
-}
+string Customer::getAddress() {return address;}
+string Customer::getTelephone() {return telephone;}
+void Customer::setAddress(string address) {this->address = address;}
+void Customer::setTelephone(string telephone) {this->telephone = telephone;}
--- Person.cpp
+++ Person.cpp
@@ -5,27 +5,9 @@ Person::Person(string firstname, string lastname, int age)
     this->lastname = lastname;
     this->age = age;
 };
-string Person::getFirstName()
-{
-    return firstname;
-}
-string Person::getLastName()
-{
-    return lastname;
-}
-int Person::getAge()
-{
-    return age;
-}
-void Person::setFirstName(string firstname)
-{
-    this->firstname = firstname;
-}
-void Person::setLastName(string lastname)
-{
-    this->lastname = lastname;
-}
-void Person::setAge(int age)
-{
-    this->age = age;
-}
+string Person::getFirstName() {return firstname;}
+string Person::getLastName() {return lastname;}
+int Person::getAge() {return age;}
+void Person::setFirstName(string firstname) {this->firstname = firstname;}
+void Person::setLastName(string lastname) {this->lastname = lastname;}
+void Person::setAge(int age) {this->age = age;}
