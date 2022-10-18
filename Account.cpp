f75269fd4d791c8fc79c0e3ac11111333eba54c7
kanokpol123 <kanokpolza32@gmail.com>
2022-10-18 17:04:44 +0700

    Account and System class

--- Account.cpp
+++ Account.cpp
@@ -1,11 +1,45 @@
 #include "Account.h"
 using namespace std;
 
-Account  getUsername(){
+string Account::getUsername(){
+
+}
+void Account::setUsername(string user){
 
 }
 
+string Account::getPassword(){
+    
+}
 
-string login(string user, string pass){
+void Account::setPassword(string pass){
+
+}
+
+string Account::getSignedDate(){
+    
+}
+
+void Account::setSignedDate(string date){
+
+}
+
+string Account::getAddress(){
+    
+}
+
+void Account::setAddress(string address){
+
+}
+
+string Account::getPrivilege(){
+    
+}
+
+void Account::setPrivilege(char privilege){
+
+}
+
+void Account::login(string user, string pass){
     
 }
