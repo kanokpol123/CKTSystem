8211745279891d68074b17832dec751a135a51c8
Minero20 <pisit.namsiri@gmail.com>
2022-10-18 15:10:51 +0700

    15.10

---
+++ Menu.cpp
@@ -0,0 +1,21 @@
+#include "Menu.h"
+void addFood(Food newFood)
+{
+
+}
+void deleteFood(int index, string foodName)
+{
+
+}
+void showSearchFood(string foodName)
+{
+
+}
+int getAmountINMenu()
+{
+
+}
+void setAmountINMenu(int amount)
+{
+    
+}
---
+++ Menu.h
@@ -0,0 +1,14 @@
+#include "Food.h"
+using namespace std;
+
+class Menu : public Food
+{
+private:
+    int amountInMenu;
+public:
+    void addFood(Food newFood);
+    void deleteFood(int index, string foodName);
+    void showSearchFood(string foodName);
+    int getAmountINMenu();
+    void setAmountINMenu(int amount);
+};
