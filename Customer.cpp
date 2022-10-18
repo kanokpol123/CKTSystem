9a769dd54e039857dd9a3febb437e11ca8462a62
PONDDDDD <pondpond22626@gmail.com>
2022-10-18 17:29:38 +0700

    17.29

--- Customer.cpp
+++ Customer.cpp
@@ -8,3 +8,20 @@ string Customer::getAddress() {return address;}
 string Customer::getTelephone() {return telephone;}
 void Customer::setAddress(string address) {this->address = address;}
 void Customer::setTelephone(string telephone) {this->telephone = telephone;}
+string Customer::regis(string username, string password, string name, string address, string telephone)
+{
+    cout << "========== Register ==========" << endl;
+    do{
+        cout << "Username: ";
+        cin >> username;
+    }while(true);
+    cout << "Password: ";
+    cin >> password;
+    cout << "Name: ";
+    cin >> name;
+    cout << "Address: ";
+    cin >> address;
+    cout << "Telephone: ";
+    cin >> telephone;
+    
+}
--- Customer.h
+++ Customer.h
@@ -1,4 +1,5 @@
 #include "Person.h"
+#include "Account.h"
 using namespace std;
 
 class Customer : public Person
@@ -6,10 +7,12 @@ class Customer : public Person
 private:
     string telephone;
     string address;
+    Account custAccount;
 public:
     Customer(string telephone, string address, string firstname, string lastname, int age);
     string getTelephone();
     void setTelephone(string telephone);
     string getAddress();
     void setAddress(string address);
+    string regis(string username, string password, string name, string address, string telephone);
 };
